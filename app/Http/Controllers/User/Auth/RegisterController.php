762b26f728dbbfe2d518434d6c755068ad736e80
dangquocdung <dungthinhvn@gmail.com>
2021-03-24 14:21:15 +0700

    asdsad

--- app/Http/Controllers/User/Auth/RegisterController.php
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Models\Manager;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
